1bde14148c72a4fd6706b3314b21963c0e897b03
aoneban <torgregion1@gmail.com>
2024-02-24 20:20:05 +0100

    feat: described the types in the app.ts file

--- news-JS-main/src/components/app/app.ts
+++ news-JS-main/src/components/app/app.ts
@@ -1,21 +1,23 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { IItemsData, ISource } from '../../types/interfaces';
 
 class App {
 
-    controller: AppController;
-    view: AppView;
+    public controller: AppController
+    public view: AppView
 
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
-        const elem = document.querySelector('.sources') as any;
-        elem.addEventListener('click', (e: any) => this.controller.getNews(e, (data: any) => this.view.drawNews(data)));
-        this.controller.getSources((data: any) => this.view.drawSources(data));
+    start(): void {
+        const elem = document.querySelector('.sources') as HTMLElement;
+        elem.addEventListener('click', (e: object) => this.controller.getNews(e, (data: IItemsData) => this.view.drawNews(data)));
+        this.controller.getSources((data: ISource) => this.view.drawSources(data));
     }
 }
 
 export default App;
+
